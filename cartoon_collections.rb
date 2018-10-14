e5732c048d35ced23f09196510bde87ac9373dec
grant smith <grantsmith1256@gmail.com>
2018-10-14 21:09:27 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -15,7 +15,7 @@ def long_planeteer_calls(array)
 end
 
 def find_the_cheese(array)
-  cheese_types = "cheddar", "gouda", "camembert"]
+  cheese_types = "cheddar", "gouda", "camembert"
   array.find do |word|
   word == cheese_types
 end
