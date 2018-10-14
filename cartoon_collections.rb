eb6122d86a2829365a188aa025def53f357ed47c
grant smith <grantsmith1256@gmail.com>
2018-10-14 20:55:59 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -8,8 +8,9 @@ def summon_captain_planet(array)
   array.collect { |w| w.capitalize + "!"}
 end
 
-def long_planeteer_calls
-  array.any?
+def long_planeteer_calls(array)
+  array.any? do |word|
+    word.length < 4
 end
 
 def find_the_cheese# code an argument here
